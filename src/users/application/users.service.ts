128d97ee3d5cb3c79589dfb2773e61e7a47dcf7f
Ilya Kozlovski <kozlvskiilya@gmail.com>
2025-04-29 15:23:00 +0300

    argon 2 comments

--- src/users/application/users.service.ts
+++ src/users/application/users.service.ts
@@ -13,7 +13,17 @@ export const userService = {
     if (!isEmailUnique) {
       return "email or login";
     }
-    const hashedPassword: string = await argon2.hash(dto.password);
+    const hashedPassword: string = await argon2.hash(dto.password,
+        { type: argon2.argon2id, timeCost: 3, parallelism: 1, memoryCost: 65536 });
+
+    /*
+    type: argon2.argon2id, // Тип Argon2 (рекомендуется argon2id)
+            memoryCost: 65536,    // 64 MB памяти
+            timeCost: 3,          // 3 раунда
+            parallelism: 1,       // 1 поток
+            saltLength: 16,       // Длина соли (по умолчанию 16 байт)
+            hashLength: 32,       // Длина хэша (по умолчанию 32 байта)
+    */
 
     const newUser: User = {
       login: dto.login,
