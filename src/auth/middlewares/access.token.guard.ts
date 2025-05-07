d66b746427e9394ed6be7ee3badfa84926fdaa36
Ilya Kozlovski <kozlvskiilya@gmail.com>
2025-05-07 17:41:14 +0300

    add access.token.guard normal middleware

--- src/auth/middlewares/access.token.guard.ts
+++ src/auth/middlewares/access.token.guard.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import {jwtService} from "../adapters/jwt.service";
-
+import {IdType} from "../../core/types/id";
 
 export const jwtTokenGuard = async (
     req: Request,
@@ -9,13 +9,13 @@ export const jwtTokenGuard = async (
 ) => {
     const header = req.headers.authorization;
 
-    // Проверка наличия заголовка Authorization
+
     if (!header) {
         res.sendStatus(401)
         return
     }
 
-    // Разбор заголовка Authorization
+
     const authHeaderParts = header.split(" ");
     if (authHeaderParts.length !== 2 || authHeaderParts[0] !== "Bearer") {
         res.sendStatus(401)
@@ -24,7 +24,7 @@ export const jwtTokenGuard = async (
 
     const [authType, token] = authHeaderParts;
 
-    // Проверка токена (пример)
+
     const payload = await jwtService.verifyToken(token);
     if (!payload) {
         res.sendStatus(401)
@@ -34,3 +34,27 @@ export const jwtTokenGuard = async (
 
     next();
 };
+
+
+
+// export const accessTokenGuard = async (req: Request, res: Response, next: NextFunction) => {
+//     if (!req.headers.authorization) return res.sendStatus(401);
+//
+//     const [authType, token] = req.headers.authorization.split(' ')[1];
+//
+//     if (authType !== 'Bearer') return res.sendStatus(401);
+//
+//     const payload = await jwtService.verifyToken(token);
+//     if (payload) {
+//         const { userId } = payload;
+//
+//         req.user = { id: userId } as IdType;
+//         next();
+//
+//         return;
+//     }
+//     res.sendStatus(401);
+//
+//     return;
+// };
+
---
+++ src/types/index.d.ts
@@ -0,0 +1,12 @@
+// src/types/index.d.ts
+
+declare global {
+    namespace Express {
+        interface Request {
+            userId?: string; // Добавляем необязательное свойство userId
+            user?: { id: string }; // Добавляем необязательное свойство user
+        }
+    }
+}
+
+export {}; // Эта строка необходима, чтобы TypeScript воспринимал файл как модуль
